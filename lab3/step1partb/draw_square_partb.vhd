50f700fcacda6e93d2df175a608b86c10ed31167
Ava Birtwistle <148493409+avabirtwistle@users.noreply.github.com>
2026-03-03 12:19:59 -0800

    Refactor VGA square drawing logic and comments

--- lab3/step1partb/draw_square_partb.vhd
+++ lab3/step1partb/draw_square_partb.vhd
@@ -18,16 +18,17 @@ use ieee.numeric_std.all;
 entity vga_square is
 	port(
 	clk: in std_logic;
-	blank_in: in std_logic;
-	hpos, vpos: in positive range 1 to 1024;
-	vga_red, vga_green, vga_blue: out std_logic_vector(3 downto 0)
+	blank_in: in std_logic; -- signal telling the display to turn off the pixel output during the horizontal and verticle blanking period
+	set_red, set_green, set_blue: in std_logic_vector(3 downto 0); -- the input switch values
+	hpos, vpos: in positive range 1 to 1024; -- input signals representing the current horizontal and vertical pixel position on the screen
+	vga_red, vga_green, vga_blue: out std_logic_vector(3 downto 0) -- 4-bit color per channel
 	);
 end vga_square;
 
-architecture logic_flow of vga_square is
-	signal size: positive range 1 to 1024:=100;
-	signal obj_X_pos: positive range 1 to 1024:=320;
-	signal obj_Y_pos: positive range 1 to 1024:=240;
+architecture logic_flow of vga_square is 
+	signal size: positive range 1 to 1024:=100;-- the size of the square in pixels (default 100)
+	signal obj_X_pos: positive range 1 to 1024:=320; -- the x position of the square on screen (default 320)
+	signal obj_Y_pos: positive range 1 to 1024:=240; -- the y position of the square on screen (default 240)
 
 begin
 
@@ -35,18 +36,18 @@ square_draw :process(clk)
 	begin
 
 	if(rising_edge(clk)) then
-	if(blank_in = '0') then
-	if    ( (0<= hpos + size - obj_X_pos)
-	    and (obj_X_pos + size-hpos >= 0)
-		and (0 <= vpos + size - obj_Y_pos)
-		and (obj_Y_pos + size - vpos >= 0) ) then
+	if(blank_in = '0') then -- if there is not blanking 
+	if    ( (0<= hpos + size - obj_X_pos) -- if the current horizontal pixel position is not too far left
+	    and (obj_X_pos + size-hpos >= 0) -- if the current position is not too far right of the square
+		and (0 <= vpos + size - obj_Y_pos) -- if vpos is not too far above the square
+		and (obj_Y_pos + size - vpos >= 0) ) then -- if vpos is not too far bellow the 
 	vga_red   <= x"f";
 	vga_green <= x"0";
 	vga_blue  <= x"0";
-	else
-	vga_red    <=x"f";
-	vga_green  <=x"f";
-	vga_blue   <=x"0";
+	else -- it is the background
+	vga_red    <=set_red; -- assign the switch color to it
+	vga_green  <=set_green;
+	vga_blue   <=set_blue;
 	end if;
 	else
 	vga_red    <=x"0";
